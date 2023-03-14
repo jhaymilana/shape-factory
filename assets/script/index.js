1fb5fde16501dab9d5f9325a1d3ad63cc16dcae7
Jayvee Milana <jmmilana12@gmail.com>
2023-03-14 10:59:25 -0500

    shape creator

--- assets/script/index.js
+++ assets/script/index.js
@@ -1,1 +1,15 @@
 'use strict';
+
+let createButton = document.querySelector('.create');
+let storage = document.querySelector('.storage');
+let shapeSelector = document.querySelector('.shape');
+let colorSelector = document.querySelector('.color');
+
+createButton.addEventListener('click', function() {
+  let newShape = document.createElement('div');
+
+  newShape.className = `${shapeSelector.value} ${colorSelector.value}`;
+  storage.appendChild(newShape);
+});
+
+
