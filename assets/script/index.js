64b124bf8084336d71c26884a16e9dbc9186b850
Jayvee Milana <jmmilana12@gmail.com>
2023-03-14 11:07:48 -0500

    storage validation

--- assets/script/index.js
+++ assets/script/index.js
@@ -4,12 +4,17 @@ let createButton = document.querySelector('.create');
 let storage = document.querySelector('.storage');
 let shapeSelector = document.querySelector('.shape');
 let colorSelector = document.querySelector('.color');
+let output = document.querySelector('.output p');
 
 createButton.addEventListener('click', function() {
-  let newShape = document.createElement('div');
+  if (storage.childElementCount <= 19) {
+    let newShape = document.createElement('div');
 
-  newShape.className = `${shapeSelector.value} ${colorSelector.value}`;
-  storage.appendChild(newShape);
+    newShape.className = `${shapeSelector.value} ${colorSelector.value}`;
+    storage.appendChild(newShape);
+  } else {
+    output.innerHTML = 'The storage is full!'
+  }
 });
 
 
